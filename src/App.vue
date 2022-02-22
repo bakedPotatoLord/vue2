75ab50e29c2f48e7e86d086b542f7fcd173eb050
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2022-02-22 19:16:58 +0000

    continued messing around with form tags

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<form action="/form">
+		<input type="text" v-model="text">
+		<input type="submit">
+	</form>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+	export default {
+  data() {
+    return {
+      data: '',
+    }
+  },
+	methods:{
+		
+	}
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
